e6a434c3c760007d8cb11ecad1a6ecc111d10041
wishjeewa88 <63997451+wishjeewa88@users.noreply.github.com>
2021-04-14 15:44:20 +0800

    Update model.py

--- model.py
+++ model.py
@@ -78,9 +78,13 @@ def build_landmark_model(input_shape, output_size):
                                     padding='valid')
 
     # Dense layers.
-    dense_1 = keras.layers.GCNConv(units=1024,
-                                 activation='relu',
-                                 use_bias=True)
+    dense_1 = keras.layers.GCNConv(input_dim=self.input_dim,
+                                            output_dim=FLAGS.hidden1,
+                                            placeholders=self.placeholders,
+                                            act=tf.nn.relu,
+                                            dropout=True,
+                                            sparse_inputs=True,
+                                            logging=self.logging))
     """dense_1 = keras.layers.Dense(GraphConvolution(input_dim=self.input_dim,
                                             output_dim=FLAGS.hidden1,
                                             placeholders=self.placeholders,
